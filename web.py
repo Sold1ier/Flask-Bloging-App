f6a0ede227a7143f63bb1c1aa9772becc2b4be93
Sold1ier <60295110+Sold1ier@users.noreply.github.com>
2020-08-10 18:41:20 +0300

    conti

--- web.py
+++ web.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>hello</h1>"
+    return render_template("index.html")
 
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:   
+        return render_template("login.html")
 
-@app.route("/<name>")
-def user(name):
-    return f"hello {name}!"
-
-@app.route("/admin")
-def admin():
-    return "<h1>ERROR you not an admin</h1>"
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
